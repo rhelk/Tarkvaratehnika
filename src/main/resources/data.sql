ae4ed0071bdb1f2c7f791575969e233030f0474a
Egle <egpalk@ttu.ee>
2019-02-21 19:48:39 +0200

    new inserts to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,7 @@ insert into users values (next value for users_sequence,
                      'Karl', 'Peresau', 'peresau@aaa.ee', 'parool');
 insert into users values (next value for users_sequence,
                      'Jill', 'Jacobs', 'jj@mail.ru', 'eiutle');
-insert into property values (next value for property_sequence, 'Time of your life', 'Something this', 'uus tn. 5','something', 3, 2, 1, 200);
+insert into property values (next value for property_sequence, 'Time of your life', 'Something this', 'uus tn. 5','http://dijkstra.cs.ttu.ee/~egpalk/tehnika/1.jpg', 3, 2, 1, 200);
+insert into property values (next value for property_sequence, 'Best room for best price', 'Lovely room', 'Tallinn','http://dijkstra.cs.ttu.ee/~egpalk/tehnika/2.jpg', 3, 2, 1, 100);
+insert into property values (next value for property_sequence, 'Have to Love this', 'Located in the heart of real nature', 'Lootuse tee','http://dijkstra.cs.ttu.ee/~egpalk/tehnika/3.jpg', 3, 2, 1, 300);
+insert into property values (next value for property_sequence, 'Good place for a company', 'Some description', 'Vanalinn','http://dijkstra.cs.ttu.ee/~egpalk/tehnika/4.jpg', 3, 2, 1, 500);
