45101a086cb2a26c7c8733868bda9a4988456897
Egle <egpalk@ttu.ee>
2019-02-27 09:05:39 +0200

    main and routes

--- web/src/main.js
+++ web/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 import Routes from './routes'
 import VueRouter from 'vue-router'
+import axios from 'axios';
+import VueAxios from 'vue-axios'
+
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+//Vue.use(VueAxios, axios);
+//Vue.prototype.$http = axios;
 
 const router = new VueRouter({
   routes: Routes,
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,7 +1,9 @@
 import allRooms from './views/allRooms.vue'
 import room from './views/room.vue'
+import addProperty from './views/addRoom.vue'
 
 export default [
   {path: "/", component: allRooms },
-  {path: '/room/:id', component: room}
+  {path: '/room/:id', component: room},
+  {path: '/add', component: addProperty},
 ]
