d17bfb640cbc0b8ca61ef7fefc721913b3e2a00a
Egle <egpalk@ttu.ee>
2019-03-11 12:24:33 +0200

    changes in main (imports)

--- web/src/main.js
+++ web/src/main.js
@@ -5,10 +5,20 @@ import Routes from './routes'
 import VueRouter from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import LoadScript from 'vue-plugin-load-script'
+import Vuelidate from 'vuelidate'
+import VueGmaps from 'vue-gmaps'
 
-
+Vue.use(Vuelidate);
+Vue.use(LoadScript);
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(VueGmaps, {
+  key: "AIzaSyA0KwxE33umTFkw6tesrmBAZ6EwqtJGgGU",
+  libraries: ['places']
+});
+
+
 //Vue.use(VueAxios, axios);
 //Vue.prototype.$http = axios;
 
