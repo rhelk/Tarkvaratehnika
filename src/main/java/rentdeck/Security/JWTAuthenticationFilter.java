2d0e604599f5130ed19af6eb285b57ba9ef0356f
Oss <oskar.oja@gmail.com>
2019-03-13 14:03:10 +0200

    JWT backendis olemas

--- src/main/java/rentdeck/Security/JWTAuthenticationFilter.java
+++ src/main/java/rentdeck/Security/JWTAuthenticationFilter.java
@@ -1,34 +1,42 @@
 package rentdeck.Security;
 
+import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.stream.Collectors;
 
-//@CrossOrigin(origins = "http://localhost:9000")
-public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static rentdeck.SecurityConstants.*;
 
-    public ApiAuthenticationFilter(String defaultFilterProcessesUrl) {
+public class JWTAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public JWTAuthenticationFilter(String defaultFilterProcessesUrl) {
+
         super(defaultFilterProcessesUrl);
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response)
-            throws AuthenticationException, IOException, ServletException {
+            throws AuthenticationException {
 
-        System.out.println("basically working...");
         LoginCredentials loginCredentials;
         try {
 //            BufferedReader buffer = new BufferedReader(new InputStreamReader(request.getInputStream()));
@@ -53,5 +61,18 @@ public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFil
                         loginCredentials.getPassword());
 
         return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = JWT.create()
+                .withSubject(((User) auth.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(SECRET.getBytes()));
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
     }
 }
--- src/main/java/rentdeck/configuration/SecurityConf.java
+++ src/main/java/rentdeck/configuration/SecurityConf.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import rentdeck.Security.ApiAuthenticationFilter;
+import rentdeck.Security.JWTAuthenticationFilter;
 import rentdeck.Security.handlers.*;
 
 import javax.servlet.Filter;
@@ -50,18 +50,9 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         http.addFilterAfter(apiLoginFilter("/api/login"), LogoutFilter.class);
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:9000"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     public Filter apiLoginFilter(String url) throws Exception {
-        ApiAuthenticationFilter filter = new ApiAuthenticationFilter(url);
+        JWTAuthenticationFilter filter = new JWTAuthenticationFilter(url);
 
         filter.setAuthenticationManager(authenticationManager());
 
