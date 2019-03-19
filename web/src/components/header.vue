17e3c00d15f2f4f5a4d5a65aea92e0191d335342
Egle <egpalk@ttu.ee>
2019-03-19 17:11:37 +0200

    header with search

--- web/src/components/header.vue
+++ web/src/components/header.vue
@@ -1,18 +1,18 @@
 <template>
-  <b-navbar >
+  <b-navbar>
     <div class="container-fluid">
       <a class="navbar-brand">RentDeck</a>
       <b-navbar>
         <b-nav-form>
-          <b-form-input class="mr-sm-2 " type="text" placeholder="Search" />
-          <b-button variant = "dark" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-form-input class="mr-sm-2 " type="text" placeholder="Search"/>
+          <b-button variant="dark" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
       </b-navbar>
       <b-navbar-nav class="ml-auto">
-          <router-link to='/' exact> Home</router-link>
-          <router-link to='/property/add' exact> Add property</router-link>
+        <router-link to='/' exact> Home</router-link>
+        <router-link to='/property/add' exact> Add property</router-link>
       </b-navbar-nav>
-      </div>
+    </div>
   </b-navbar>
 </template>
 
