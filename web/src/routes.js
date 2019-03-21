a31ea7223c9e82db466e898d0c6099a76a9ffdcd
Egle <egpalk@ttu.ee>
2019-03-21 20:19:04 +0200

    new route

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,13 +4,16 @@ import addProperty from './views/addRoom.vue'
 import login from './views/login.vue'
 import register from "./views/register"
 import home from "./views/home.vue"
+import allRoomsSearch from './views/allRoomsSearch.vue'
 
 
 export default [
+  {path: "/:id", component: allRooms },
   {path: "/", component: allRooms },
   {path: '/room/:id', component: room},
   {path: '/property/add', component: addProperty},
   {path: '/login', component: login},
   {path: '/register', component: register},
   {path: '/home', component: home},
+
 ]
