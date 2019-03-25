3ad41e1786d69a076c47a2bf381bb6af18c6d286
rohelk <rohelk@ttu.ee>
2019-03-26 01:30:32 +0200

    AuthorityDao added, also added printouts to follow

--- src/main/java/rentdeck/dao/AuthorityDao.java
+++ src/main/java/rentdeck/dao/AuthorityDao.java
@@ -1,7 +1,15 @@
 package rentdeck.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import rentdeck.model.Users;
 
 public interface AuthorityDao extends JpaRepository<Users, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO authorities VALUES (?1, 'ROLE_USER'))", nativeQuery = true)
+    void addUserToUserRole(String username);
 }
