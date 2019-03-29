ff8fac52467d81a2d2cf8a168eddb2eeab6f74a4
Egle <egpalk@ttu.ee>
2019-03-29 15:03:56 +0200

    data with correct pic url and better addresses

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,22 +5,22 @@ insert into users values (next value for users_sequence,
 insert into property (property_id, title, description, address, county, municipality, settlement,
                       street, pic_url, room_count, bed_count, owner_id, price)
        values (next value for property_sequence, 'Time of your life', 'Something this', 'uus tn. 5',
-                      'Har County', 'Mar municipialty', 'Sar settlement', 'Var street',
-                      'http://dijkstra.cs.ttu.ee/~egpalk/tehnika/1.jpg', 3, 2, 1, 200);
+                      'Harjumaa', 'Tallinn', 'Nõmme', 'Mai',
+                      'https://firebasestorage.googleapis.com/v0/b/tarkvaratehnika-1551709647803.appspot.com/o/images%2Ffdkjgflkfdmlkgmlkfjygkjtrkjgkrkgmn%2Cds%2Cf%2C.jpg?alt=media&token=6b67b60c-6316-4145-8115-89010a7be338', 3, 2, 1, 200);
 insert into property (property_id, title, description, address, county, municipality, settlement,
                       street, pic_url, room_count, bed_count, owner_id, price)
        values (next value for property_sequence, 'Best room for best price', 'Lovely room', 'Tallinn',
-                      'Hir County', 'Mir municipialty', 'Sir settlement', 'Vir street',
-                      'http://dijkstra.cs.ttu.ee/~egpalk/tehnika/2.jpg', 4, 2, 1, 100);
+                      'Harjumaa', 'Tallinn', 'Kadriorg', 'Vesivärava',
+                      'https://firebasestorage.googleapis.com/v0/b/tarkvaratehnika-1551709647803.appspot.com/o/images%2Ffkjdskfnkjdsngkjnejgnjdfnj.jpg?alt=media&token=8d8e84a0-de92-4c49-a8c9-84f32136d77d', 4, 2, 1, 100);
 insert into property (property_id, title, description, address, county, municipality, settlement,
                       street, pic_url, room_count, bed_count, owner_id, price)
-       values (next value for property_sequence, 'Have to Love this', 'Located in the heart of real nature',
-                      'Her County', 'Mer municipialty', 'Ser settlement', 'Ver street',
-                      'Lootuse tee','http://dijkstra.cs.ttu.ee/~egpalk/tehnika/3.jpg', 3, 2, 1, 300);
+       values (next value for property_sequence, 'Have to Love this', 'Located in the heart of real nature','Lootuse tee',
+                      'Tartumaa', 'Tartu', 'Annelinn', 'Sõbra',
+                      'https://firebasestorage.googleapis.com/v0/b/tarkvaratehnika-1551709647803.appspot.com/o/images%2Ffsdkfjmksdmngknfskdjgnkjnsdkjgfnkfdmn.jpg?alt=media&token=1a27b033-33f3-4ffb-b821-9657b239eafc', 3, 2, 1, 300);
 insert into property (property_id, title, description, address, county, municipality, settlement,
                       street, pic_url, room_count, bed_count, owner_id, price)
        values (next value for property_sequence, 'Good place for a company', 'Some description', 'Vanalinn',
-                      'Hor County', 'Mor municipialty', 'Sor settlement', 'Vor street',
-                      'http://dijkstra.cs.ttu.ee/~egpalk/tehnika/4.jpg', 3, 2, 1, 500);
+                      'Harjumaa', 'Tallinn', 'Mustamäe', 'Vilde tee',
+                      'https://firebasestorage.googleapis.com/v0/b/tarkvaratehnika-1551709647803.appspot.com/o/images%2Fvbgfhbgfnjfksdjkfjlkdmhkbgfkhmlkgfmhgflkhmlkgf.jpg?alt=media&token=ea650c20-420e-47b4-889b-a8237f160de4', 3, 2, 1, 500);
 INSERT INTO authorities VALUES ('peresau', 'ROLE_USER');
 INSERT INTO authorities VALUES ('jj', 'ROLE_ADMIN');
