4d380c078a10eff1009589cea5e0ce46b8c2df32
Egle <egpalk@ttu.ee>
2019-05-04 12:46:57 +0300

    disabled dates in calendar, also made more responsive

--- web/src/views/room.vue
+++ web/src/views/room.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class=container-fluid style="margin-top: 20px">
+    <div class=container style="margin-top: 20px">
         <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-6">
+            <div class="col-lg-8 col-md-12 col-xs-6">
                 <b-img :src="roomDetails.pic_url" fluid alt="Responsive image"/>
+                <h1 style="margin-top: 20px">{{roomDetails.title}}</h1>
+                <p> <b>Location:</b> {{roomDetails.county + ", " + roomDetails.municipality + ", " + roomDetails.settlement + "," +
+                    roomDetails.street }}</p>
+                <p><b>Number of rooms:</b> {{roomDetails.room_count}}</p>
+                <p><b>Number of beds:</b> {{roomDetails.bed_count}}</p>
+                <p><b>Property description:</b> {{roomDetails.description}}</p>
+
             </div>
-            <div id="price" class="col-md-4">
+            <div id="price" class="col-lg-4 col-md-8 col-xs-12">
                 <h2>Price</h2>
                 <p>Price per night: {{roomDetails.price}}</p>
                 <HotelDatePicker v-if="userCanRent" style="margin-bottom: 30px" format="DD/MM/YYYY"
                                  @check-in-changed="setCheckinDate"
                                  @check-out-changed="setCheckOutDate"
                                  :disabledDates="disabledDates"
+                                 :key="disabledDates.length"
                 >
                 </HotelDatePicker>
                 <b-button v-if="userCanRent" v-bind:disabled="hasClicked" variant="dark" class="my-2 my-sm-0"
@@ -19,19 +26,6 @@
                           v-on:click.prevent="rentProperty">Rent
                 </b-button>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div id="houseDetails" class="col-md-6">
-                <h1>{{roomDetails.title}}</h1>
-                <p>Location: {{roomDetails.county + ", " + roomDetails.municipality + ", " + roomDetails.settlement + ", " +
-                    roomDetails.street }}</p>
-                <p>Rooms: {{roomDetails.room_count}}</p>
-                <p>Beds: {{roomDetails.bed_count}}</p>
-                <p>Description: {{roomDetails.description}}</p>
-
-            </div>
-            <div class="col-md-4"></div>
         </div>
     </div>
 
@@ -59,7 +53,7 @@
         data: function () {
             return {
                 date: '',
-                disabledDates: [], //['2019-04-29', ...]
+                disabledDates: [], //['2019-05-05', '2019-05-07']
                 checkIn: this.startingDateValue,
                 checkOut: this.endingDateValue,
                 userCanRent: true,
@@ -70,16 +64,31 @@
         },
         created() {
             const that = this;
+            //DISABLE DATES
+            this.$store.dispatch('doGet', {url: 'rent/dates/' + this.id}).then(data => {
+                for (let i = 0; i < data.data.length; i++) {
+                    let start = new Date(data.data[i].start);
+                    let end = new Date(data.data[i].end);
+                    for (start; start <= end; start.setDate(start.getDate() + 1)) {
+                        let month = (parseInt(start.getMonth()) + 1).toString();
+                        let day = start.getDate().toString();
+                        if (month.length < 2) month = '0' + month;
+                        if (day.length < 2) day = '0' + day;
+                        this.disabledDates.push(start.getFullYear() + "-" + month + "-" + day);
+                    }
+                }
+            });
+
+            //GET PROPERTY DATA
             this.$store.dispatch('doGet', {url: 'property/get/' + this.id}).then(data => {
                 that.roomDetails = data.data;
                 if (data.data.users.user_id === this.$store.getters.getUser_id) {
                     that.userCanRent = false;
-                    console.log(that.userCanChange)
                 }
-            })
-
+            });
 
         },
+
         methods: {
             rentProperty: function () {
                 this.hasClicked = true;
@@ -96,32 +105,31 @@
                             }
 
                     }).then(data => {
-                        console.log("tehtud");
                         this.$router.push({path: `/all`});
-                        //this.$router.go();
+
                     })
                 }
 
 
             },
             setCheckinDate(date) {
-                if(date != null) {
+                if (date != null) {
                     let month = (parseInt(date.getMonth()) + 1).toString();
                     let day = date.getDate().toString();
                     if (month.length < 2) month = '0' + month;
                     if (day.length < 2) day = '0' + day;
                     this.checkIn = date.getFullYear() + "-" + month + "-" + day;
-                    console.log(this.checkIn);
+
                 }
             },
             setCheckOutDate(date) {
-                if(date != null) {
+                if (date != null) {
                     let month = (parseInt(date.getMonth()) + 1).toString();
                     let day = date.getDate().toString();
                     if (month.length < 2) month = '0' + month;
                     if (day.length < 2) day = '0' + day;
                     this.checkOut = date.getFullYear() + "-" + month + "-" + day;
-                    console.log(this.checkOut);
+
                 }
             },
         }
@@ -129,30 +137,16 @@
 </script>
 
 <style scoped>
-    #roomProfile {
-        align-content: center;
-        width: 100%;
-        max-width: 1000px;
-        box-sizing: border-box;
-        margin: auto;
-        padding: 20px;
-    }
 
-    img {
-        width: 90%;
-    }
-
-    #houseDetails {
-        margin: auto;
-        padding: 20px;
-    }
 
     #price {
-        max-width: 350px;
         background: whitesmoke;
         height: 250px;
-        margin-top: 60px;
 
+    }
+
+    h2 {
+        font-family: Arial;
     }
 
 </style>
